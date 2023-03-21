847aae5d64d18c26bbb05fd8d6ecdfc6a54db8be
hennaoj <hennaojala99@gmail.com>
2023-03-21 21:46:28 +0200

    small fix

--- review_system/constants.py
+++ review_system/constants.py
@@ -1,4 +1,4 @@
 LINK_RELATIONS_URL = "/review_system/link-relations/"
 MOVIE_PROFILE = "/profiles/movie/"
-REVIEW_PROFILE = "/profiles/movie/"
+REVIEW_PROFILE = "/profiles/review/"
 MASON = "application/vnd.mason+json"
